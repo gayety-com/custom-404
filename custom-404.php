3d78742fbb982e001c9420160a4ef1ad9ad97e0d
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-09-10 15:27:07 -0700

    please work

--- custom-404.php
+++ custom-404.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Gayety 000 Custom 404 Redirect to Home
+Plugin Name: Gayety Custom 404 Redirect to Home
 Description: Redirects all 404 errors to the home page and shows a message "That page does not exist."
 Version: 1.0
 Author: Jeff Kaufman
